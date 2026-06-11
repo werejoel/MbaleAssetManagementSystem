cc2e0b3eeaeba7657657b66e147e00bedb65ca8a
werejoel <joelbank481@gmail.com>
2026-06-11 23:47:08 +0300

    nnn

--- routes/assignments.js
+++ routes/assignments.js
@@ -7,8 +7,8 @@ const assignmentController = createCrudControllers("asset_assignments", ["asset_
 
 router.get("/", authenticateToken, assignmentController.getAll);
 router.get("/:id", authenticateToken, assignmentController.getById);
-router.post("/", authenticateToken, authorizeRoles("admin", "asset_manager", "store_manager", "technician", "staff"), assignmentController.create);
-router.put("/:id", authenticateToken, authorizeRoles("admin", "asset_manager", "store_manager", "technician", "staff"), assignmentController.update);
-router.delete("/:id", authenticateToken, authorizeRoles("admin", "asset_manager", "store_manager"), assignmentController.delete);
+router.post("/", authenticateToken, authorizeRoles("admin", "asset_manager", "store_manager", "technician", "staff","department_head"), assignmentController.create);
+router.put("/:id", authenticateToken, authorizeRoles("admin", "asset_manager", "store_manager", "technician", "staff","department_head"), assignmentController.update);
+router.delete("/:id", authenticateToken, authorizeRoles("admin", "asset_manager", "store_manager","department_head"), assignmentController.delete);
 
 module.exports = router;
